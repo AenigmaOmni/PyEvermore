68d1e43830ab12615305c721bb0a235dcb84a3f5
SolarOmni <johnathan.crocker@outlook.com>
2021-02-16 21:43:33 -0600

    Added scaling

--- src/game.py
+++ src/game.py
@@ -8,6 +8,9 @@ class Game:
         self.on_init(width, height, title)
         self.level = Level()
         self.lastTime = 0
+        self.cameraScale = 3
+        self.cameraX = 64
+        self.cameraY = 64
 
     def run(self):
         while self.running:
@@ -31,7 +34,9 @@ class Game:
 
     def render(self):
         self.screen.fill((0,0,0))
-        self.level.render(self.screen)
+        self.level.render(self.backBuffer)
+        tmp = pygame.transform.scale(self.backBuffer, self.size)
+        self.screen.blit(tmp, (0,0))
         pygame.display.flip()
 
     def cleanUp(self):
@@ -42,3 +47,4 @@ class Game:
         self.size = self.width, self.height = width, height
         self.screen = pygame.display.set_mode(self.size, pygame.HWSURFACE | pygame.DOUBLEBUF)
         pygame.display.set_caption(title)
+        self.backBuffer = pygame.Surface((340, 256), pygame.HWSURFACE | pygame.DOUBLEBUF)
