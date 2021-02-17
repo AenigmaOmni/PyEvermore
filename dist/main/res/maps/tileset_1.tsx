1952272e1f4b96c0b7f36dea22449b2e2f800d9c
SolarOmni <johnathan.crocker@outlook.com>
2021-02-17 08:53:34 -0600

    Changed ignore

--- dist/main/res/maps/tileset_1.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.3" name="tileset_1" tilewidth="32" tileheight="32" tilecount="144" columns="8">
- <image source="tileset-2pointo.png" width="256" height="600"/>
-</tileset>
